ee83b61b40b07018cdc4c47dc202a6acf8ef4442
Kees Leune <kees@leune.org>
2024-04-07 20:03:41 -0400

    Create settings.py
    Added config file

---
+++ settings.py
@@ -0,0 +1,4 @@
+FOLLOWUP_LABEL = "01-Follow Up"             # Gmail label to send summary of
+FROM = '"<Your Name>" user@example.com'     # This person will SEND the summary
+TO = 'user@example.com'                     # This person will receive it. Email address only!
+STYLE = 'font-family: monospace; padding-right:15px'    # format the HTML part of the message
